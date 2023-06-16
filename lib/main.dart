46134d6635162f71b1c9d095fc1629e2a558a850
ikhwamulhakim <ikhwanulhakim43@gmail.com>
2023-06-16 22:45:06 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:tugas2/Page/details_page.dart';
+import 'package:tugas2/Page/error_page.dart';
+import 'package:tugas2/Page/explore_page.dart';
+import 'package:tugas2/Page/first_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Cozy',
+      theme: ThemeData(
+      ),
+      home: const FirstPage()
+    );
+  }
+}
